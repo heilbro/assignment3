1757e73b52309106f2df602657c6b5ce1afc0c6b
Michael Heilbronner <michaelheilbronner2025@u.northwestern.edu>
2024-05-20 00:04:11 +0000

    update again

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,19 +3,20 @@ class PlacesController < ApplicationController
     @places = Place.new
 end
 
-def index 
-  @places = Places.all
-end
-
-def show
-  @places = places.find_by ({ "id" => params ["id"]})
-
 def create
   
   @places = Places.new
   @places ["name"] = params ["name"]
   @places.save
   redirect_to "/places"
+
+  def index 
+    @places = Places.all
+  end
+  
+  def show
+    @places = places.find_by ({ "id" => params ["id"]})
+    @entries
 
 
 
